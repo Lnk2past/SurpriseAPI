f847c0a4fd77f7b9273665ade4644bf7676b9610
Lnk2past <Lnk2past@gmail.com>
2019-09-19 23:18:44 -0400

    happy birthday!

--- surpriseapi/generator.py
+++ surpriseapi/generator.py
@@ -11,14 +11,24 @@ def get_operation():
     return r.choice(operations)
 
 
-def get_endpoint():
-    n = namegenerator.get_random_name()
-    return f'/{n}'
+def get_endpoint(path_probability, path_input_probability):
+    n = f'/{namegenerator.get_random_name()}'
+    r = random.SystemRandom().random()
+    while r > path_probability:
+        if r > path_input_probability:
+            n += f'/<{namegenerator.get_random_left()}>'
+        else:
+            n += f'/{namegenerator.get_random_right()}'
+        r = random.SystemRandom().random()
+    return n
 
 
 def parse_inputs():
     parser = argparse.ArgumentParser(__file__)
     parser.add_argument('--endpoint-count', default=10)
+    parser.add_argument('--path-probability', default=0.60)
+    parser.add_argument('--path-input-probability', default=0.80)
+    
     return parser.parse_args()
 
 
@@ -27,7 +37,7 @@ def main():
     app = Flask(__name__)
     api = Api(app)
     for i in range(inputs.endpoint_count):
-        n = get_endpoint()
+        n = get_endpoint(inputs.path_probability, inputs.path_input_probability)
         supers = (Resource,)
         attrs = {get_operation(): lambda: None}
         e = api.route(n)(type(n, supers, attrs))
